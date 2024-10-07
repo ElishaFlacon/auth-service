7eb0ebbfe9d67447512f337b6f803f412d2bef4b
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-10-08 00:23:35 +0500

    chore: add linter, formatter, gitignore

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module shelly
 
 go 1.22.1
+
+require (
+	gorm.io/driver/postgres v1.5.9
+	gorm.io/gorm v1.25.12
+)
--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var (
+	a int = 12
+	b int = 2
+)
+
 func main() {
-	fmt.Println("Hello, Shelly!")
+	fmt.Println("Этот код был сгенерирован!asdasdasdsad ", a+b)
 }
